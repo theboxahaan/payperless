f40fa9cd0dab48e5e4bb1014331cf071b21883cf
Ahaan Dabholkar <ahaandabholkar@gmail.com>
2020-02-06 23:21:30 +0530

    dashboard-front-end

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 
 
@@ -12,7 +12,12 @@ def serve_login_page():
 def login():
     if request.method == 'POST':
         user, pwd = request.form['username'], request.form['password']
-        return user+' '+pwd
+        return redirect(url_for('dashboard'))
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
     app.run(debug='True')
